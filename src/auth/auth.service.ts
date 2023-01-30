27b6bdf450ce2f7fed6577ea73576377c1f5a5b1
JyR Bucao <108910158+Renato-Questnova@users.noreply.github.com>
2023-01-30 16:36:42 +0800

    Authentication

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,102 @@
+import { Injectable } from '@nestjs/common';
+import { sign, verify } from 'jsonwebtoken';
+import { User } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
+import RefreshToken from './entities/refresh-token.entity';
+
+@Injectable()
+export class AuthService {
+  private refreshTokens: RefreshToken[] = [];
+
+  constructor(private readonly userService: UsersService) {}
+
+  async refresh(refreshStr: string): Promise<string | undefined> {
+    const refreshToken = await this.retrieveRefreshToken(refreshStr);
+    if (!refreshToken) {
+      return undefined;
+    }
+    const user = await this.userService.findOne(refreshToken.userId);
+    if (!user) {
+      return undefined;
+    }
+    const accessToken = {
+      userId: refreshToken.userId,
+    };
+    return sign(accessToken, process.env.ACCESS_SECRET, { expiresIn: '1h' });
+  }
+  private retrieveRefreshToken(
+    refreshStr: string,
+  ): Promise<RefreshToken | undefined> {
+    try {
+      const decode = verify(refreshStr, process.env.REFRESH_SERCRET);
+      if (typeof decode === 'string') {
+        return undefined;
+      }
+      return Promise.resolve(
+        this.refreshTokens.find(
+          (token: RefreshToken) => token.id === decode.id,
+        ),
+      );
+    } catch (e) {
+      return undefined;
+    }
+  }
+
+  async login(
+    email: string,
+    password: string,
+    values: { userAgent: string; ipAddress: string },
+  ): Promise<{ accessToken: string; refreshToken: string } | undefined> {
+    const user = await this.userService.findByEmail(email);
+    if (!user) {
+      return undefined;
+    }
+    /**
+     * ! Verify your user -- use argon2 for password hashing!!
+     */
+    if (user.password !== password) {
+      return undefined;
+    }
+    return this.newRefreshAndAccessToken(user, values);
+  }
+  private async newRefreshAndAccessToken(
+    user: User,
+    values: { userAgent: string; ipAddress: string },
+  ): Promise<{ accessToken: string; refreshToken: string }> {
+    const refreshObject = new RefreshToken({
+      id:
+        this.refreshTokens.length === 0
+          ? 0
+          : this.refreshTokens[this.refreshTokens.length - 1].id + 1,
+      ...values,
+      userId: user.id,
+    });
+    this.refreshTokens.push(refreshObject);
+
+    return {
+      refreshToken: refreshObject.sign(),
+      accessToken: sign(
+        {
+          userId: user.id,
+        },
+        process.env.ACCESS_SECRET,
+        {
+          // expireIn: '1h',
+        },
+      ),
+    };
+  }
+  async logout(refreshStr): Promise<void> {
+    const refreshToken = await this.retrieveRefreshToken(refreshStr);
+
+    if (!refreshToken) {
+      return;
+    }
+    /**
+     * ! Delete refresh token from db
+     */
+    this.refreshTokens = this.refreshTokens.filter(
+      (refreshToken: RefreshToken) => refreshToken.id !== refreshToken.id,
+    );
+  }
+}
